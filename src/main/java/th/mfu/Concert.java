2ad757f91e6d0c7899c91557d94dcc2f579c5917
Kritsasoft <6531503005@lamduan.mfu.ac.th>
2023-09-11 16:30:19 +0700

    GG

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,21 +5,55 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
-
+    private String title;
+    private String description;
+    private Date date;
+    private int id;
+    private String performer;
     public Concert() {
     }
     public Concert(String title, String description) {
         //TODO: set attributes
+        this.title = title;
+        this.description = description;
     }
  
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
     public String getTitle() {
         //TODO: return title
-        return "";
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
     }
     public String getDescription() {
         //TODO: return description
-        return "";
+        return description;
     }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void put(int id2, Concert concert) {
+    }
+    public String getPerformer(){
+        return performer;
+    }
+    public void setPerformer(String performer){
+        this.performer = performer;
+    }
+
+
 
     //TODO: add getters and setters
 
